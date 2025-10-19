d3b12cb34e568a38f2086e245ef7aaa72bbecf60
Tiago Antunes Bugalho <antunesbugalhot@gmail.com>
2025-10-19 14:14:04 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "tigro-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "js/jsQR.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
